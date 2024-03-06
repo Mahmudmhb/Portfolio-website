4b22c87b8c1e5992f4c9d07bff8f53f0cf78a02e
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-03-06 16:24:28 +0600

    contact page update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Contact = () => {
+  const form = useRef();
+  // const emailname = form.current.name.value;
+  // const email = form.current.email.value;
+  // console.log(emailname, email);
+  // const subjcet = form.current.subjcet.value;
+  // const message = form.current.message.value;
+  // const current = { emailname, email, subjcet, message };
+  // console.log(current);
+  // console.log(currentemail);
+  const sendEmail = (event) => {
+    event.preventDefault();
+    // console.log(event);
+    emailjs
+      .sendForm("service_ov1golo", "template_3lsztov", form.current, {
+        publicKey: "eNMOg_xGl5v-nx2Wy",
+      })
+      .then(
+        () => {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your Email has been SuccessFully",
+            showConfirmButton: false,
+            timer: 2000,
+          });
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
   return (
     <div className="w-5/6 mt-20 mx-auto">
       <div className="md:flex gap-4 items-center justify-between">
@@ -51,30 +84,30 @@ const Contact = () => {
           </div>
         </div>
 
-        <form className="w-full">
+        <form ref={form} onSubmit={sendEmail} className="w-full">
           <div className="space-y-10">
             <div className="flex gap-4 ">
               <input
                 type="text"
                 placeholder="Name"
-                name=""
+                name="name"
                 className="bg-[#1d1d1d] w-full rounded py-3 text-white"
               />
               <input
                 type="email"
                 placeholder="Email"
-                name=""
+                name="email"
                 className="bg-[#1d1d1d] w-full rounded  py-3 text-white"
               />
             </div>
             <input
               type="text"
               placeholder="Subject"
-              name=""
+              name="subject"
               className="bg-[#1d1d1d] w-full  py-3 rounded text-white"
             />
             <textarea
-              name=""
+              name="message"
               id=""
               rows="5"
               //   cols="10"
@@ -83,7 +116,10 @@ const Contact = () => {
             ></textarea>
           </div>
 
-          <button className="w-full rounded-3xl mt-10 border border-[#8954f7] py-3 hover:text-white text-[#8954f7] text-xl hover:bg-[#8954f7]">
+          <button
+            type="submit"
+            className="w-full  rounded-3xl mt-10 border border-[#8954f7] py-3 hover:text-white text-[#8954f7] text-xl hover:bg-[#8954f7]"
+          >
             Lets Talk
           </button>
         </form>
@@ -93,3 +129,47 @@ const Contact = () => {
 };
 
 export default Contact;
+
+// const sendEmail = (e) => {
+//   e.preventDefault();
+
+//   emailjs.sendForm('mahmud', 'template_syu6dnq', form.current, 'user_yES1kTZV9ujsBfNf3w7Qg')
+//     .then((result) => {
+//          alert('Message sent successfully!');
+//     }, (error) => {
+//         alert(error.message);
+//     });
+//     e.target.reset();
+// };
+
+// return (
+//     <div id="contact" className='text-center background py-5'>
+//         <h2 className="fh-bold text-white ">CONTACT <span className="text-primary">ME</span></h2>
+
+//             <div className="row">
+//                 <div className=" col-12 col-md-6">
+//                     <div>
+//                         <img class="img-fluid"  src={image} alt="" />
+//                     </div>
+//                 </div>
+//                 <div className="col-12 col-md-6">
+
+//                 <div className=" mt-5  pt-5">
+//                     <form action="" ref={form} onSubmit={sendEmail}>
+//                             <input type="text" class=" mb-3 rounded-3 w-75 py-1 " name="name" id="exampleFormControlInput1" placeholder="Your name" required/><br />
+//                             <input type="email" class="mb-3 py-1 w-75 rounded-3" name="email" id="exampleFormControlInput1" placeholder="Your E-mail" required/> <br />
+
+//                             <textarea class=" py-1 rounded-3 w-75 mb-3" required id="exampleFormControlTextarea1" name="message" placeholder="Your Message" rows="3"></textarea>
+//                             <br />
+
+//                             <input type="submit" className="btn btn-primary w-75 rounded-3 " value="SUBMIT"/>
+//                     </form>
+
+//         </div>
+
+//                 </div>
+
+//             </div>
+//     </div>
+// );
+// };
--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Navber = () => {
   return (
     <div className=" border-b">
       <div className="md:flex justify-between w-5/6 mx-auto  text-white items-center h-20">
-        <div className=" text-white">
-          <h1>Mahmudul hasan</h1>
+        <div className=" text-white sm:text-center">
+          <h1 className="sm:text-center">Mahmudul hasan</h1>
         </div>
-        <ul className="flex   md:justify-between gap-10  items-center ">
+        <ul className="flex   md:justify-between md:gap-10 gap-2  items-center ">
           <Link>
             {" "}
             <li>home</li>
